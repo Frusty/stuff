ccfe1c1c18aae7c62c6314a4140a7f524d82b571
Oscar <oscarmpp@gmail.com>
2011-01-14 12:48:31 +0100

    mejoras en chute.pl

--- scripts/chute.pl
+++ scripts/chute.pl
@@ -22,27 +22,27 @@ Args:
 \t--command, -c\tOptional, can be repeated. Commands to send, use quotes for space-separated arguments.
 \t\t\tIf no commands specified, it will use "show config".
 \t--file\t\tOptional. Print the command output to a file.
-Example: $0 management.jare.es -c "show version" -c "show interface status" -c "show interface trunk"
+Example: $0 jare.es -c "show version" -c "show interface status" -c "show interface trunk"
 EOF
 exit 1;
 }
 
 my %chuis =();
-$chuis{'.1.3.6.1.4.1.2467.4.7'}        = {name => 'CSS11501',         user => 'XXXXX', pass => 'XXXXX', runcmd => 'terminal length 65535', defcmd => 'show running-config'};
-$chuis{'.1.3.6.1.4.1.9.1.359'}         = {name => 'WS-C2950T-24',     user => 'XXXXX', pass => 'XXXXX', runcmd => 'terminal length 0', enable => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.9.1.559'}         = {name => 'WS-C2950T-48-SI',  user => 'XXXXX', pass => 'XXXXX', runcmd => 'terminal length 0', enable => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.9.1.716'}         = {name => 'WS-C2960-24TT-L',  user => 'XXXXX', pass => 'XXXXX', runcmd => 'terminal length 0', enable => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.9.1.717'}         = {name => 'WS-C2960-48TT-L',  user => 'XXXXX', pass => 'XXXXX', runcmd => 'terminal length 0', enable => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.9.1.696'}         = {name => 'WS-C2960G-24TC-L', user => 'XXXXX', pass => 'XXXXX', runcmd => 'terminal length 0', enable => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.5624.2.2.220'}    = {name => 'C2H124-48',        user => 'XXXXX', pass => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.5624.2.2.286'}    = {name => 'C2H124-48P',       user => 'XXXXX', pass => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.5624.2.1.100'}    = {name => 'B3G124-24',        user => 'XXXXX', pass => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.5624.2.1.53'}     = {name => '7H4382-25',        user => 'XXXXX', pass => 'XXXXX'};
-$chuis{'.1.3.6.1.4.1.5624.2.1.59'}     = {name => '1H582-25',         user => 'XXXXX', pass => 'XXXXX',  runcmd => 'set terminal rows disable'};
-$chuis{'.1.3.6.1.4.1.5624.2.1.34'}     = {name => '1H582-51',         user => 'XXXXX', pass => 'XXXXX',  runcmd => 'set terminal rows disable'};
-$chuis{'.1.3.6.1.4.1.2636.1.1.1.2.31'} = {name => 'ex4200-24p',       user => 'XXXXX', pass => 'XXXXX',  runcmd => 'set cli screen-length 0'};
+$chuis{'.1.3.6.1.4.1.2467.4.7'}        = {name => 'CSS11501',         user => 'XXXX', pass => 'XXXX' , runcmd => 'terminal length 65535', defcmd => 'show running-config'};
+$chuis{'.1.3.6.1.4.1.9.1.359'}         = {name => 'WS-C2950T-24',     user => 'XXXX', pass => 'XXXX' , runcmd => 'terminal length 0', enable => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.9.1.559'}         = {name => 'WS-C2950T-48-SI',  user => 'XXXX', pass => 'XXXX' , runcmd => 'terminal length 0', enable => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.9.1.716'}         = {name => 'WS-C2960-24TT-L',  user => 'XXXX', pass => 'XXXX' , runcmd => 'terminal length 0', enable => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.9.1.717'}         = {name => 'WS-C2960-48TT-L',  user => 'XXXX', pass => 'XXXX' , runcmd => 'terminal length 0', enable => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.9.1.696'}         = {name => 'WS-C2960G-24TC-L', user => 'XXXX', pass => 'XXXX' , runcmd => 'terminal length 0', enable => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.5624.2.2.220'}    = {name => 'C2H124-48',        user => 'XXXX', pass => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.5624.2.2.286'}    = {name => 'C2H124-48P',       user => 'XXXX', pass => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.5624.2.1.100'}    = {name => 'B3G124-24',        user => 'XXXX', pass => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.5624.2.1.53'}     = {name => '7H4382-25',        user => 'XXXX', pass => 'XXXX'};
+$chuis{'.1.3.6.1.4.1.5624.2.1.59'}     = {name => '1H582-25',         user => 'XXXX', pass => 'XXXX',  runcmd => 'set terminal rows disable'};
+$chuis{'.1.3.6.1.4.1.5624.2.1.34'}     = {name => '1H582-51',         user => 'XXXX', pass => 'XXXX',  runcmd => 'set terminal rows disable'};
+$chuis{'.1.3.6.1.4.1.2636.1.1.1.2.31'} = {name => 'ex4200-24p',       user => 'XXXX', pass => 'XXXX',  runcmd => 'set cli screen-length 0'};
 
-my @communities = qw/public XXXXX YYYYY/;
+my @communities = qw/public/;
 
 sub getSNMP() {
     my $machine   = shift || 'localhost';
@@ -124,22 +124,22 @@ foreach my $host (@ARGV) {
         $telnet->cmd($chuis{$sysObjectID}{runcmd});
     }
 
+    my $filename = "${host}-".strftime("%Y%m%d-%H%M%S", localtime).".txt";
+    $filename =~ s/[^\.\w:-]/_/g;
     my $defcmd = $chuis{$sysObjectID}{defcmd} || "show config";
     @cmds = ("$defcmd") unless @cmds;
+    open(LOG, ">> $filename") || print BOLD RED "#\tCan't open '$filename'" if $file;
+
     foreach (@cmds) {
         s/[\n\t\f]+/ /g;
         print CYAN "#\tExecuting '$_' on '$host'\n\n";
-        print "${prompt}$_\n";
-    my $output = join('', $telnet->cmd("$_"));
-        print $output;
+        my $output = join('', $telnet->cmd("$_"));
+        print "${prompt}$_\n$output";
         print CYAN "#\tEOF '$_' on '$host'.\n";
-    if ($file) {
-            my $filename = "${host}-$_-".strftime("%Y%m%d_%H%M%S", localtime);
-        $filename =~ s/[^\w:-]/_/g;
-            print GREEN "#\tWriting output on '$filename.txt'.\n";
-            open(LOG, "> ${filename}.txt") || die "Can't redirect stdout";
-            print LOG $output;
-            close(LOG);
+        if ($file) {
+            print GREEN "#\tWriting output on '$filename'.\n";
+            print LOG "${prompt}$_\n$output";
         }
     }
+    close(LOG);
 }

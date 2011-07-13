ede4ed1c0a63177c04b22b2e77ff87b01d4df01f
Oscar <oscarmpp@gmail.com>
2011-07-13 09:59:15 +0200

    updates y fixes a archiso

--- archiso/build.sh
+++ archiso/build.sh
@@ -22,32 +22,52 @@ get_linux_ver() {
 
 # Base installation (root-image)
 make_basefs() {
-    mkarchiso ${verbose} -C /proc/$$/fd/3 -D "${install_dir}" -p "base" create "${work_dir}"
-    mkarchiso ${verbose} -C /proc/$$/fd/3 -D "${install_dir}" -p "memtest86+ syslinux" create "${work_dir}"
+    mkarchiso ${verbose} -D "${install_dir}" -p "base base-devel" create "${work_dir}"
+    mkarchiso ${verbose} -D "${install_dir}" -p "memtest86+ syslinux mkinitcpio-nfs-utils nbd" create "${work_dir}"
 }
 
 # Additional packages (root-image)
 make_packages() {
-    ok "Update local repository"
+    ok "Update local custom repository"
     for i in custompkgs/*xz; do repo-add custompkgs/custompkgs.db.tar.gz $i; done
-    mkarchiso ${verbose} -C /proc/$$/fd/3 -D "${install_dir}" -p "$(grep -v ^# packages.i686 | xargs) $(find custompkgs/ -name '*.pkg.tar.xz' | sed -n 's/.*\/\([0-z]\+\)-.*/\1 /p' | xargs)" create "${work_dir}"
+    arch_packages=$(grep -v ^# packages.i686 | xargs)
+    custom_packages=$(find custompkgs/ -name '*.pkg.tar.xz' | sed -n 's/.*\/\([0-z]\+\)-.*/\1 /p' | xargs)
+    mkarchiso ${verbose} -C /proc/$$/fd/3 -D "${install_dir}" -p "$arch_packages $custom_packages" create "${work_dir}"
 }
 
 # Customize installation (root-image)
-make_customize_root_image() {
+make_root_image() {
     if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
         cp -af root-image ${work_dir}
         chmod 750 ${work_dir}/root-image/etc/sudoers.d
         chmod 440 ${work_dir}/root-image/etc/sudoers.d/g_wheel
-        if [[ ! -L ${work_dir}/root-image/etc/mtab ]]; then
-            rm -f ${work_dir}/root-image/etc/mtab
-        fi
         mkdir -p ${work_dir}/root-image/etc/pacman.d
         wget -O ${work_dir}/root-image/etc/pacman.d/mirrorlist http://www.archlinux.org/mirrorlist/all/
         sed -i "s/#Server/Server/g" ${work_dir}/root-image/etc/pacman.d/mirrorlist
+        chroot ${work_dir}/root-image /usr/sbin/locale-gen
+        ok "Making the default user arch"
+        chroot ${work_dir}/root-image /usr/sbin/useradd -p "" -u 2000 -g users -G "audio,disk,optical,wheel,log" arch
+        ok "Chown/chmod the arch folder"
+        chroot ${work_dir}/root-image /bin/chown -R arch:users /home/arch
+        chroot ${work_dir}/root-image /bin/chmod -R 700 /home/arch
+        ok "Making the default user crypt"
+        chroot ${work_dir}/root-image /usr/sbin/useradd -p "" -u 1000 -g users -G "audio,disk,optical,wheel,log" crypt
+        ok "Backup our archiso structure"
+    tar -cjvf ${work_dir}/root-image/archiso.bz2 --exclude={archiso.bz2,*.iso,${PWD}/work} "$PWD"
         : > ${work_dir}/build.${FUNCNAME}
-	tar -cjvf ${work_dir}/root-image/archiso.bz2 --exclude={archiso.bz2,*.iso,${PWD}/work} "$PWD"
     fi
+}
+
+# Copy mkinitcpio archiso hooks (root-image)
+make_setup_mkinitcpio() {
+   if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
+        local _hook
+        for _hook in archiso archiso_pxe_nbd archiso_loop_mnt; do
+            cp /lib/initcpio/hooks/${_hook} ${work_dir}/root-image/lib/initcpio/hooks
+            cp /lib/initcpio/install/${_hook} ${work_dir}/root-image/lib/initcpio/install
+        done
+        : > ${work_dir}/build.${FUNCNAME}
+   fi
 }
 
 # Prepare ${install_dir}/boot/
@@ -56,8 +76,8 @@ make_boot() {
         local _src=${work_dir}/root-image
         local _dst_boot=${work_dir}/iso/${install_dir}/boot
         mkdir -p ${_dst_boot}/${arch}
-        cp ${_src}/boot/vmlinuz26 ${_dst_boot}/${arch}
-        mkinitcpio -c ./mkinitcpio.conf -b ${_src} -k $(get_linux_ver) -g ${_dst_boot}/${arch}/archiso.img
+        mkinitcpio -c ./mkinitcpio.conf -b ${_src} -k /boot/vmlinuz26 -g ${_dst_boot}/${arch}/archiso.img
+        mv ${_src}/boot/vmlinuz26 ${_dst_boot}/${arch}
         cp ${_src}/boot/memtest86+/memtest.bin ${_dst_boot}/memtest
         cp ${_src}/usr/share/licenses/common/GPL2/license.txt ${_dst_boot}/memtest.COPYING
         : > ${work_dir}/build.${FUNCNAME}
@@ -71,16 +91,13 @@ make_syslinux() {
         local _dst_syslinux=${work_dir}/iso/${install_dir}/boot/syslinux
         mkdir -p ${_dst_syslinux}
         sed "s|%ARCHISO_LABEL%|${iso_label}|g;
-            s|%INSTALL_DIR%|${install_dir}|g;
-            s|%ARCH%|${arch}|g" syslinux/syslinux.cfg > ${_dst_syslinux}/syslinux.cfg
+             s|%INSTALL_DIR%|${install_dir}|g;
+             s|%ARCH%|${arch}|g" syslinux/syslinux.cfg > ${_dst_syslinux}/syslinux.cfg
         cp syslinux/splash.png ${_dst_syslinux}
-        cp ${_src_syslinux}/*.c32 ${_dst_syslinux}
-        cp ${_src_syslinux}/*.com ${_dst_syslinux}
-        cp ${_src_syslinux}/*.0 ${_dst_syslinux}
-        cp ${_src_syslinux}/memdisk ${_dst_syslinux}
+        cp ${_src_syslinux}/{*.c32,*.com,*.0,memdisk} ${_dst_syslinux}
         mkdir -p ${_dst_syslinux}/hdt
         wget -O - http://pciids.sourceforge.net/v2.2/pci.ids | gzip -9 > ${_dst_syslinux}/hdt/pciids.gz
-        cat ${work_dir}/root-image/lib/modules/$(get_linux_ver)/modules.alias | gzip -9 > ${_dst_syslinux}/hdt/modalias.gz
+        cat ${work_dir}/root-image/lib/modules/*-ARCH/modules.alias | gzip -9 > ${_dst_syslinux}/hdt/modalias.gz
         : > ${work_dir}/build.${FUNCNAME}
     fi
 }
@@ -150,13 +167,14 @@ if [[ $# -gt 0 ]]; then
     rm -f *-${arch}.iso
 fi
 
-ok make_pacman_conf && make_pacman_conf
-ok make_basefs && make_basefs
-ok make_packages && make_packages
-ok make_customize_root_image && make_customize_root_image
-ok make_boot && make_boot
-ok make_syslinux && make_syslinux
-ok make_isolinux && make_isolinux
-ok make_aitab && make_aitab
-ok make_prepare && make_prepare
-ok make_iso && make_iso
+ok make_pacman_conf      && make_pacman_conf
+ok make_basefs           && make_basefs
+ok make_packages         && make_packages
+ok make_root_image       && make_root_image
+ok make_setup_mkinitcpio && make_setup_mkinitcpio
+ok make_boot             && make_boot
+ok make_syslinux         && make_syslinux
+ok make_isolinux         && make_isolinux
+ok make_aitab            && make_aitab
+ok make_prepare          && make_prepare
+ok make_iso              && make_iso

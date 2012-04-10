cb573c8a66aa0d780b68ec3a5bdbe020fb23029a
Oscar <oscarmpp@gmail.com>
2012-04-10 14:35:20 +0200

    Archiso update

--- archiso/build.sh
+++ archiso/build.sh
@@ -35,19 +35,19 @@ __EOF__
 
 # Base installation (root-image)
 make_basefs() {
-    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "base" create
-    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "memtest86+ syslinux mkinitcpio-nfs-utils nbd curl" create
+    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "base" init
+    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "memtest86+ syslinux mkinitcpio-nfs-utils nbd curl" install
 }
 
 # Additional packages (root-image)
 make_packages() {
-    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "$(grep -v ^# ${script_path}/packages.${arch})" create
+    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "$(grep -v ^# ${script_path}/packages.${arch})" install
 }
 
 make_custom_packages() {
     for i in custompkgs/*xz; do repo-add ${script_path}/custompkgs/custompkgs.db.tar.gz $i; done
-    custom_packages=$(find ${script_path}/custompkgs/ -name '*.pkg.tar.xz' | sed -n 's/.*\/\([0-z]\+\)-.*/\1 /p' | xargs)
-    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "$custom_packages" create
+    custom_packages=$(find ${script_path}/custompkgs/ -name '*.pkg.tar.xz' | sed -n 's/.*\/\([-0-z]\+\)-.*/\1 /p' | xargs)
+    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" -p "$custom_packages" install
 }
 
 # Copy mkinitcpio archiso hooks (root-image)
@@ -55,12 +55,12 @@ make_setup_mkinitcpio() {
    if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
         local _hook
         for _hook in archiso archiso_shutdown archiso_pxe_common archiso_pxe_nbd archiso_pxe_http archiso_pxe_nfs archiso_loop_mnt; do
-            cp /lib/initcpio/hooks/${_hook} ${work_dir}/root-image/lib/initcpio/hooks
-            cp /lib/initcpio/install/${_hook} ${work_dir}/root-image/lib/initcpio/install
+            cp /usr/lib/initcpio/hooks/${_hook} ${work_dir}/root-image/usr/lib/initcpio/hooks
+            cp /usr/lib/initcpio/install/${_hook} ${work_dir}/root-image/usr/lib/initcpio/install
         done
-        cp /lib/initcpio/install/archiso_kms ${work_dir}/root-image/lib/initcpio/install
-        cp /lib/initcpio/archiso_shutdown ${work_dir}/root-image/lib/initcpio
-        cp /lib/initcpio/archiso_pxe_nbd ${work_dir}/root-image/lib/initcpio
+        cp /usr/lib/initcpio/install/archiso_kms ${work_dir}/root-image/usr/lib/initcpio/install
+        cp /usr/lib/initcpio/archiso_shutdown ${work_dir}/root-image/usr/lib/initcpio
+        cp /usr/lib/initcpio/archiso_pxe_nbd ${work_dir}/root-image/usr/lib/initcpio
         cp ${script_path}/mkinitcpio.conf ${work_dir}/root-image/etc/mkinitcpio-archiso.conf
         : > ${work_dir}/build.${FUNCNAME}
    fi
@@ -72,7 +72,9 @@ make_boot() {
         local _src=${work_dir}/root-image
         local _dst_boot=${work_dir}/iso/${install_dir}/boot
         mkdir -p ${_dst_boot}/${arch}
-        mkarchroot -n -r "mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/archiso.img" ${_src}
+        mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" \
+            -r 'mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/archiso.img' \
+            run
         mv ${_src}/boot/archiso.img ${_dst_boot}/${arch}/archiso.img
         mv ${_src}/boot/vmlinuz-linux ${_dst_boot}/${arch}/vmlinuz
         cp ${_src}/boot/memtest86+/memtest.bin ${_dst_boot}/memtest
@@ -125,8 +127,13 @@ make_customize_root_image() {
         mkdir -p ${work_dir}/root-image/etc/pacman.d
         wget -O ${work_dir}/root-image/etc/pacman.d/mirrorlist http://www.archlinux.org/mirrorlist/all/
         sed -i "s/#Server/Server/g" ${work_dir}/root-image/etc/pacman.d/mirrorlist
-        chroot ${work_dir}/root-image /usr/sbin/locale-gen
-        chroot ${work_dir}/root-image /usr/sbin/useradd -m -p "" -g users -G "audio,disk,optical,wheel" arch
+        sed -i 's/#\(en_US\.UTF-8\)/\1/' ${work_dir}/root-image/etc/locale.gen
+        mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" \
+            -r 'locale-gen' \
+            run
+        mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" \
+            -r 'useradd -m -p "" -g users -G "audio,disk,optical,wheel" arch' \
+            run
         : > ${work_dir}/build.${FUNCNAME}
     fi
 }
@@ -143,71 +150,6 @@ make_customize_root_image_2() {
         chroot ${work_dir}/root-image /bin/chown -R arch:users /home/arch
 #        chroot ${work_dir}/root-image /usr/sbin/useradd -m -p "" -u 1000 -g users -G "audio,disk,optical,wheel,log" crypt
         tar -cjvf ${work_dir}/root-image/archiso.bz2 --exclude={archiso.bz2,*.iso,${PWD}/work} ${script_path}
-        : > ${work_dir}/build.${FUNCNAME}
-    fi
-}
-
-# Copy mkinitcpio archiso hooks (root-image)
-make_setup_mkinitcpio() {
-   if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
-        local _hook
-        for _hook in archiso archiso_shutdown archiso_pxe_common archiso_pxe_nbd archiso_pxe_http archiso_pxe_nfs archiso_loop_mnt; do
-            cp /lib/initcpio/hooks/${_hook} ${work_dir}/root-image/lib/initcpio/hooks
-            cp /lib/initcpio/install/${_hook} ${work_dir}/root-image/lib/initcpio/install
-        done
-        cp /lib/initcpio/install/archiso_kms ${work_dir}/root-image/lib/initcpio/install
-        cp /lib/initcpio/archiso_shutdown ${work_dir}/root-image/lib/initcpio
-        cp /lib/initcpio/archiso_pxe_nbd ${work_dir}/root-image/lib/initcpio
-        cp ${script_path}/mkinitcpio.conf ${work_dir}/root-image/etc/mkinitcpio-archiso.conf
-        : > ${work_dir}/build.${FUNCNAME}
-   fi
-}
-
-# Prepare ${install_dir}/boot/
-make_boot() {
-    if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
-        local _src=${work_dir}/root-image
-        local _dst_boot=${work_dir}/iso/${install_dir}/boot
-        mkdir -p ${_dst_boot}/${arch}
-        mkarchroot -n -r "mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/archiso.img" ${_src}
-        mv ${_src}/boot/archiso.img ${_dst_boot}/${arch}/archiso.img
-        mv ${_src}/boot/vmlinuz-linux ${_dst_boot}/${arch}/vmlinuz
-        cp ${_src}/boot/memtest86+/memtest.bin ${_dst_boot}/memtest
-        cp ${_src}/usr/share/licenses/common/GPL2/license.txt ${_dst_boot}/memtest.COPYING
-        : > ${work_dir}/build.${FUNCNAME}
-    fi
-}
-
-# Prepare /${install_dir}/boot/syslinux
-make_syslinux() {
-    if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
-        local _src_syslinux=${work_dir}/root-image/usr/lib/syslinux
-        local _dst_syslinux=${work_dir}/iso/${install_dir}/boot/syslinux
-        mkdir -p ${_dst_syslinux}
-        for _cfg in ${script_path}/syslinux/*.cfg; do
-            sed "s|%ARCHISO_LABEL%|${iso_label}|g;
-                 s|%INSTALL_DIR%|${install_dir}|g;
-                 s|%ARCH%|${arch}|g" ${_cfg} > ${_dst_syslinux}/${_cfg##*/}
-        done
-        cp ${script_path}/syslinux/splash.png ${_dst_syslinux}
-        cp ${_src_syslinux}/*.c32 ${_dst_syslinux}
-        cp ${_src_syslinux}/*.com ${_dst_syslinux}
-        cp ${_src_syslinux}/*.0 ${_dst_syslinux}
-        cp ${_src_syslinux}/memdisk ${_dst_syslinux}
-        mkdir -p ${_dst_syslinux}/hdt
-        wget -O - http://pciids.sourceforge.net/v2.2/pci.ids | gzip -9 > ${_dst_syslinux}/hdt/pciids.gz
-        cat ${work_dir}/root-image/lib/modules/*-ARCH/modules.alias | gzip -9 > ${_dst_syslinux}/hdt/modalias.gz
-        : > ${work_dir}/build.${FUNCNAME}
-    fi
-}
-
-# Prepare /isolinux
-make_isolinux() {
-    if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
-        mkdir -p ${work_dir}/iso/isolinux
-        sed "s|%INSTALL_DIR%|${install_dir}|g" ${script_path}/isolinux/isolinux.cfg > ${work_dir}/iso/isolinux/isolinux.cfg
-        cp ${work_dir}/root-image/usr/lib/syslinux/isolinux.bin ${work_dir}/iso/isolinux/
-        cp ${work_dir}/root-image/usr/lib/syslinux/isohdpfx.bin ${work_dir}/iso/isolinux/
         : > ${work_dir}/build.${FUNCNAME}
     fi
 }
@@ -241,6 +183,7 @@ make_aitab() {
 
 # Build all filesystem images specified in aitab (.fs .fs.sfs .sfs)
 make_prepare() {
+    mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" pkglist
     mkarchiso ${verbose} -C /proc/$$/fd/3 -w "${work_dir}" -D "${install_dir}" prepare
 }
 

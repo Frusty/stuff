8ef6a9bab2fbf11fd232be7641d0c8babd8d9517
Oscar <oscarmpp@gmail.com>
2012-05-29 16:38:22 +0200

    +some utility scrits

---
+++ scripts/chk_spare.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+# Chech a securestack stack for spare members (48 fe free ports).
+
+use strict;
+use Net::Telnet;
+
+print "$0 <IP>\n" and exit 3 unless $#ARGV == 0 and $ARGV[0] =~ /^(\d{1,3}\.){3}\d{1,3}$/;
+
+my $telnet = new Net::Telnet ( Timeout  => 5
+                             , Errmode  => 'return'
+                             , Prompt   => '/\(rw\)->/i'
+                             );
+$telnet->open($ARGV[0]);
+$telnet->login('XXXX', 'XXXXXXXX');
+$telnet->cmd('set length 0');
+
+my @result = $telnet->cmd('show port status');
+print "Null output!\n" and exit 1 unless @result;
+chomp @result;
+
+my %hash=();
+my $count=0;
+
+foreach (@result) {
+    if (/^fe\.(\d)\.\d+\s+Down\s/) {
+    $count++;
+        $hash{$1}=1 if $count == 48;
+    } else {
+        $count = 0;
+    }
+}
+
+if (scalar keys %hash) {
+    print "OK! Hay ".(scalar keys %hash)." switch/es spares en el stack: ".join(', ', sort keys %hash)."\n";
+    exit 1;
+} else {
+    print "NOK! No hay swiches spares en el stack!\n";
+    exit 0;
+}

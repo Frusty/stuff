70ad700808d2071084a94bb3a34dbe3c5eb0369d
Oscar <oscarmpp@gmail.com>
2013-02-21 12:00:38 +0100

    redone nagios_parse

--- scripts/nagios_parse.pl
+++ scripts/nagios_parse.pl
@@ -1,111 +1,100 @@
 #!/usr/bin/perl
-# Parsea cierta info de un directorio de configuración de nagios en una estructura de datos.
+# Prints service info from negios hosts
 # http://nagios.sourceforge.net/docs/3_0/configobject.html
 # http://nagios.sourceforge.net/docs/3_0/objectinheritance.html
 
 use strict;
-use warnings;
 use Data::Dumper;
-use File::Find;
+use Term::ANSIColor qw(:constants);
+$Term::ANSIColor::AUTORESET = 1; # It doesn't seem to work everywhere.
 
-my $cfgdir     = "/opt/local/nagios/etc/";
-my %result     = ();
-my %commands   = ();
-my %hostgroups = ();
+my $cfg = "/opt/local/nagios/etc/nagios.cfg";
+my %nagios_conf = ();
 my %resources  = ();
-my @services   = ();
+my %objects = ();
 
-find(\&wanted, $cfgdir);
-sub wanted {
-    if (-f and /\.cfg$/) {
-        my $file = $File::Find::name;
-        local(*DB, $/);
-        open (DB, '<', "$file") or die "No puedo abir $file";
-        my $slurp = <DB>;
-        if ($file =~ /resource.cfg$/) {
-            while ($slurp =~ /^\s*?(\$USER\d*\$)\s*=\s*(.*)$/gm) {
-                $resources{$1} = $2;
-            }
-        }
-        while ($slurp =~ /^\s*define (\w+)\s*{([^{]+)}/gm) {
-            my ($type, $data)  = ($1, $2);
-            my $ref;
-            $data =~ s/#.*$//g;
-            while ($data =~ /^\s+(\S+)\s+(.+?)\s*$/gm) {
-                my ($first, $second) = ($1, $2);
-                next if $first =~ /#/;
-                if ($first =~ /^(?:contact_groups|host|host_name|hostgroup|hostgroup_name|members)$/) {
-                    $second =~ s/\s//g;
-                    $ref->{$first} = [split (",", $second)];
-                } else {
-                    $ref->{$first} = $second;
-                }
-            }
-            next if exists $ref->{register} and $ref->{register} =~ /0/;
-            if ($type eq 'hostgroup') {
-                foreach (@{$ref->{hostgroup_name}}) {
-                    $hostgroups{$_}{members} = [@{$ref->{members}}];
-                    $hostgroups{$_}{alias}   = $ref->{alias};
-                }
-            }
-            if ($type eq 'serviceextinfo') {
-                push (@{$ref->{hostgroup_name}}, @{$ref->{hostgroup}}) if $ref->{hostgroup};
-                map {$hostgroups{$_}{services}{$ref->{service_description}}{notes_url} = $ref->{notes_url}} @{$ref->{hostgroup_name}};
-                map {$result{$_}{services}{$ref->{service_description}}{notes_url} = $ref->{notes_url}} @{$ref->{host_name}};
-            }
-            if ($type eq 'host') {
-                $result{${$ref->{host_name}}[0]} = { use     => $ref->{use}
-                                                   , address => $ref->{address}
-                                                   , alias   => $ref->{alias}
-                                                   }
-            }
-            if ($type eq 'command') {
-                $commands{$ref->{command_name}} = $ref->{command_line};
-            }
-            if ($type eq 'service') {
-                push (@services, $ref);
-            }
-        }
+sub slurpfile ($) {
+    my $file = shift;
+    local(*INPUT, $/);
+    open (INPUT, '<', $file) or die "Error opening '$file': $!";
+    my $slurp = <INPUT>;
+    close(INPUT);
+    return $slurp;
+}
+
+sub prt ($$$) {
+    my $tabs  = shift || 1;
+    my $key   = shift || return;
+    my $value = shift || return;
+    print YELLOW "\t" x ($tabs).$key;
+    print BLUE " => ";
+    print "'$value'\n";
+}
+
+sub buildcmd ($) {
+    my $cmd  = shift;
+    my $host = shift;
+    my @check_args = split (/!/, $cmd);
+    $cmd = $objects{command}{$check_args[0]}{command_line};
+    $cmd =~ s/(\$USER\d*\$)/$resources{$1}/g;
+    $cmd =~ s/\$HOSTADDRESS\$/$objects{host}{$host}{address}/g;
+    $cmd =~ s/\$ARG(\d)\$/$check_args[$1]/g;
+    return $cmd;
+}
+
+# Build an array with the nagios.cfg params.
+my $slurp = &slurpfile($cfg);
+$nagios_conf{$1} = $2 while $slurp =~ /^([^\n=#]+?)\s*=\s*([^\n]+)\s*$/gm;
+
+my $ocf = $nagios_conf{object_cache_file};
+my $resfile = $nagios_conf{resource_file};
+
+# Build an array with the resurce.cfg params.
+$slurp = &slurpfile($resfile);
+$resources{$1} = $2 while $slurp =~ /^\s*?(\$USER\d*\$)\s*=\s*(.*)$/gm;
+
+# Build an array with the objects.cache elements.
+$slurp = &slurpfile($ocf);
+while ($slurp =~ /^define\s(\w+)\s{([^{]+)}/gm) {
+    my ($type, $data) = ($1, $2);
+    my %array = ();
+    my ($oname, $hostname);
+    while ($data =~ /^\s+(\S+)\s+(.+?)\s*$/gm) {
+        my ($first, $second) = ($1, $2);
+        $array{$first} = $second;
+        $oname = $second if $first =~ /(${type}_(?:name|description))/;
+        $hostname = $second if $first =~ /host_name/;
+    }
+    if ($hostname) {
+        $type eq 'host' ? $objects{$type}{$oname} = \%array : $objects{$type}{$hostname}{$oname} = \%array;
+    } else {
+        $objects{$type}{$oname} = \%array;
     }
 }
 
-foreach my $serv (@services) {
-    if ($serv->{hostgroup_name}) {
-        foreach my $hn (@{$serv->{hostgroup_name}}) {
-            foreach (@{$hostgroups{$hn}{members}}) {
-                if ($hostgroups{$hn}{services}{$serv->{service_description}}{notes_url}) {
-                    $result{$_}{services}{$serv->{service_description}}{notes_url} = $hostgroups{$hn}{services}{$serv->{service_description}}{notes_url};
-                }
-            }
-            push (@{$serv->{host_name}}, @{$hostgroups{$hn}{members}});
-        }
-    }
-    my %hosts = ();
-    my $cmd = "";
-    map {$hosts{$_} = 1} @{$serv->{host_name}};
-    @{$serv->{host_name}} = grep { !/^!/ } grep { not $hosts{"!$_"} } keys %hosts;
-    $serv->{check_command} and $serv->{check_command} =~ /^([^!]*)(?:|!.*)$/ and $cmd=$1;
-    foreach my $hostname (@{$serv->{host_name}}) {
-        my $cmdstring = $commands{$cmd} or next;
-        my @cmdarray = split (/!/, $serv->{check_command});
-        $cmdstring =~ s/(\$USER\d*\$)/$resources{$1}/g;
-        $cmdstring =~ s/\$HOSTADDRESS\$/$result{$hostname}{address}/g;
-	my $count = 0;
-	while ($cmdstring =~ /\$ARG(\d)\$/g) { 
-		$count++;
-		unless (defined $cmdarray[$1]) {
-		    print "Problema en el chequeo '$serv->{service_description}' del host $hostname:\n";
-		    print "\$ARGV${1}\$ no tiene un valor asociado en la definición del servicio\n";
-        	    print "check_command => $cmdstring\n";
-        	    print "command_line  => ".join ("|", @cmdarray)."\n\n";
-    		    $cmdarray[$1] = "ERROR";
-		}
-	}
-        $cmdstring =~ s/\$ARG(\d)\$/$cmdarray[$1]/g;
-        $result{$hostname}{services}{$serv->{service_description}}{check_command} = $serv->{check_command};
-        $result{$hostname}{services}{$serv->{service_description}}{$cmd} = $commands{$cmd};
-        $result{$hostname}{services}{$serv->{service_description}}{CMD}  = $cmdstring;
+# Print the info we want for each argument/host.
+foreach my $host(@ARGV) {
+    print BOLD RED "Host '$host' does not exist.\n" and next unless $objects{host}{$host};
+    print BOLD GREEN "$host\n";
+    my %result = %{$objects{host}{$host}};
+    print BOLD CYAN "\thost\n";
+    &prt(2, 'address', $result{address});
+    &prt(2, 'alias', $result{alias});
+    &prt(2, 'check_command', $result{check_command});
+    &prt(2, $result{check_command}, $objects{command}{$result{check_command}}{command_line});
+    &prt(2, 'exec_command', &buildcmd($result{check_command}, $host));
+    &prt(2,'notes_url', $result{notes_url});
+    print BOLD CYAN "\tservices\n";
+    foreach my $service (sort keys %{$objects{service}{$host}}) {
+        print GREEN "\t\t$service\n";
+        &prt(3, 'alias', $objects{service}{$host}{$service}{alias});
+        &prt(3, 'check_command', $objects{service}{$host}{$service}{check_command});
+        my @check_args = split (/!/, $objects{service}{$host}{$service}{check_command});
+        &prt(3, $check_args[0], $objects{command}{$check_args[0]}{command_line});
+        &prt(3, 'exec_command', &buildcmd($objects{service}{$host}{$service}{check_command}, $host));
+        &prt(3, 'notes_url', $objects{service}{$host}{$service}{notes_url});
     }
 }
 
-map {print Dumper $result{$_}} @ARGV or print Dumper %result;
+# Print all the objects if not argument was used.
+print Dumper \%objects unless @ARGV;

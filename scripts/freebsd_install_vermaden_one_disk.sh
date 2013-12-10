6ed25e399acb9d9296ad07916dc775ef25391740
Oscar <oscarmpp@gmail.com>
2013-12-10 10:05:18 +0100

    nt_hores now fills the entire current week

--- scripts/freebsd_install_vermaden_one_disk.sh
+++ scripts/freebsd_install_vermaden_one_disk.sh
@@ -2,6 +2,7 @@
 # http://forums.freebsd.org/showthread.php?t=31662
 
 # Partitioning
+#DISKS="ada0 ada1"
 DISKS="ada0"
 for I in ${DISKS}; do
 NUMBER=$( echo ${I} | tr -c -d '0-9' )
@@ -13,6 +14,7 @@ NUMBER=$( echo ${I} | tr -c -d '0-9' )
 done
 
 # ZFS
+# zpool create -f -o cachefile=/tmp/zpool.cache sys mirror /dev/gpt/sys*
 zpool create -f -o cachefile=/tmp/zpool.cache sys /dev/gpt/sys*
 zpool set feature@lz4_compress=enabled sys
 zfs set compression=lz4 sys
@@ -51,7 +53,7 @@ EOF
 cat << EOF >> /mnt/etc/rc.conf
 hostname="freebs92"
 ifconfig_em0="SYNCDHCP"
-#ifconfig_em1="inet 192.168.2.1 netmask 255.255.255.0 mtu 7418"
+#ifconfig_em1="xn0 192.168.2.1 netmask 255.255.255.0"
 zfs_enable="YES"
 keymap="spanish.iso.acc"
 tmpmfs="YES"
@@ -68,10 +70,11 @@ sendmail_msp_queue_enable="NO"
 named_enable="NO"
 clear_tmp_enable="YES"
 ntpd_enable="NO"
-ntpd_sync_on_start="NO"
 EOF
-echo 'PermitRootLogin no' >> /mnt/etc/ssh/sshd_config
-echo 'PermitEmptyPasswords no' >> /mnt/etc/etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /mnt/etc/ssh/sshd_config
+echo 'PermitEmptyPasswords yes' >> /mnt/etc/ssh/sshd_config
+echo 'security.bsd.see_other_uids=0' >> /mnt/etc/sysctl.conf
+echo 'security.bsd.see_other_gids=0' >> /mnt/etc/sysctl.conf
 :> /mnt/etc/fstab
 
 # Clean and reboot
